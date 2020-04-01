edd850e21c9475b3ec1696d6b41a29af5baf454d
meres <meres@xubuntu.local>
2020-04-01 17:40:14 +0200

    feature add unit tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,59 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController trainController;
+    TrainUser trainUser;
+    TrainSensorImpl trainSensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        trainController = mock(TrainController.class);
+        trainUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(trainController, trainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void BelowAbsoluteMargin() {
+        trainSensor.overrideSpeedLimit(-1);
+        verify(trainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void AboveAbsoluteMargin() {
+        trainSensor.overrideSpeedLimit(501);
+        verify(trainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void CorrectSpeedMargin() {
+        trainSensor.overrideSpeedLimit(1);
+        verify(trainUser, times(1)).setAlarmState(false);
+    }
+
+    @Test
+    public void SpeedLimitTest() {
+        assertEquals(5, trainSensor.getSpeedLimit());
+    }
+
+    @Test
+    public void BelowSafeSpeed() {
+        when(trainController.getReferenceSpeed()).thenReturn(120);
+        trainSensor.overrideSpeedLimit(50);
+        verify(trainUser, times(1)).setAlarmState(true);
     }
 }
